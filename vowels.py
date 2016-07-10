2ecbb574a522e60afe4d811817b9712b0d6443eb
Densingmoses123 <densingmoses123@gmail.com>
2016-07-10 16:36:55 +0530

    vowels.py

---
+++ vowels.py
@@ -0,0 +1,5 @@
+char c=""
+if(c=='a' or c=='e' or c=='i' or c=='o' or c=='u' or c=="A" or c=="E" or c=="I" or c=="O" or c=="U"):
+    print("Character is a vowel")
+else:
+    print("Character is a consonant")
